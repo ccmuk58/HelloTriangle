1019c982ee82d8dd83cc54151df47d39489a1ff5
Nakuri <ccmuk58@naver.com>
2026-08-18 01:37:06 +0900

    GL: add vertex color attribute

--- OpenGLHelloTriangle/OpenGLHelloTriangle/main.cpp
+++ OpenGLHelloTriangle/OpenGLHelloTriangle/main.cpp
@@ -129,9 +129,9 @@ int main()
     int vertexColorLocation = glGetUniformLocation(shaderProgram, "uniColor");
 
     float vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-        0.0f, 0.5f, 0.0f,
+        -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+        0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
+        0.0f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f,
         // 0.5f, 0.5f, 0.0f
     };
 
@@ -153,12 +153,18 @@ int main()
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+
+    // position attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
 
     // can unbind VBO
     glBindBuffer(GL_ARRAY_BUFFER, 0);
-    
+    glBindBuffer(GL_ARRAY_BUFFER, 1);
+
     // can't unbind EBO
     // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
     
--- OpenGLHelloTriangle/OpenGLHelloTriangle/shaders/fragment.glsl
+++ OpenGLHelloTriangle/OpenGLHelloTriangle/shaders/fragment.glsl
@@ -1,10 +1,9 @@
 #version 330 core
 
+in vec3 ourColor;
 out vec4 FragColor;
-
-uniform vec4 uniColor;
 
 void main()
 {
-    FragColor = uniColor;
+    FragColor = vec4(ourColor, 1.0);
 }
