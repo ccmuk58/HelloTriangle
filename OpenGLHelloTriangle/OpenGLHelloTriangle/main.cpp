9abcd615dc115c27eb39bf71a74b7f4bbea6164f
Nakuri <ccmuk58@naver.com>
2026-08-17 20:23:12 +0900

    GL: add index buffer

--- OpenGLHelloTriangle/OpenGLHelloTriangle/main.cpp
+++ OpenGLHelloTriangle/OpenGLHelloTriangle/main.cpp
@@ -97,21 +97,39 @@ int main()
     float vertices[] = {
         -0.5f, -0.5f, 0.0f,
         0.5f, -0.5f, 0.0f,
-        0.0f, 0.5f, 0.0f
+        0.0f, 0.5f, 0.0f,
+        // 0.5f, 0.5f, 0.0f
     };
 
-    unsigned int VAO, VBO;
+    unsigned int indices[] = {
+        0, 1, 2,
+        // 1, 2, 3
+    };
+
+    unsigned int VAO, VBO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
 
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
+    // can unbind VBO
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    
+    // can't unbind EBO
+    // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+    
+    // can unbind VAO
+    glBindVertexArray(0);
 
     // rendering loop
     while (!glfwWindowShouldClose(window))
@@ -123,7 +141,9 @@ int main()
 
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        //glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
